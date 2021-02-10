884a68303850d5f7bb44bed66f92ec3d7b68e338
erichon <erichon18@gmail.com>
2021-02-11 00:58:09 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 basic.forever(function () {
-    if (input.isGesture(Gesture.Shake)) {
+    if (input.buttonIsPressed(Button.A)) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
         basic.showString("" + (randint(1, 6)))
     }
 })
